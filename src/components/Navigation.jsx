7b05c36e96bb27c28eb38caf6b2bc105938f7902
OJ423 <osmith.dpm@gmail.com>
2024-10-11 14:47:49 +0100

    Change order of li in navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,8 +1,15 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import Link from "next/link";
-import { FcFlashOn, FcPhoneAndroid, FcFile, FcSearch, FcAddImage, FcPositiveDynamic } from "react-icons/fc";
+import {
+  FcFlashOn,
+  FcPhoneAndroid,
+  FcFile,
+  FcSearch,
+  FcAddImage,
+  FcPositiveDynamic,
+} from "react-icons/fc";
 import { BsWordpress } from "react-icons/bs";
 import { TbBrandGithub, TbBrandLinkedin, TbMail } from "react-icons/tb";
 import { FaShopify } from "react-icons/fa";
@@ -11,14 +18,13 @@ import { usePathname } from "next/navigation";
 import { IoClose } from "react-icons/io5";
 import { useState } from "react";
 
-
 export default function Navigation() {
-  const pathname = usePathname()
-  const [menuOpen, setMenuOpen] = useState(false)
+  const pathname = usePathname();
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const handleOpenMenu = () => {
-    setMenuOpen(!menuOpen)
-  }
+    setMenuOpen(!menuOpen);
+  };
 
   return (
     <>
@@ -38,13 +44,25 @@ export default function Navigation() {
             OJSWEB
           </p>
         </Link>
-        {!menuOpen ? 
-          <FiAlignRight onClick={handleOpenMenu} size={42} className="z-50 fixed top-4 right-4 p-1 bg-teal-100 rounded-lg"/>
-          :
-          <IoClose onClick={handleOpenMenu} size={42} className="z-50 fixed top-4 right-4 p-1 bg-teal-100 rounded-lg"/>        
-        }
+        {!menuOpen ? (
+          <FiAlignRight
+            onClick={handleOpenMenu}
+            size={42}
+            className="z-50 fixed top-4 right-4 p-1 bg-teal-100 rounded-lg"
+          />
+        ) : (
+          <IoClose
+            onClick={handleOpenMenu}
+            size={42}
+            className="z-50 fixed top-4 right-4 p-1 bg-teal-100 rounded-lg"
+          />
+        )}
       </section>
-      <header className={`${!menuOpen ? 'translate-x-[-100%] md:translate-x-0': 'translate-x-0'} transition-all duration-500 bg-white w-full bottom-0 left-0 fixed md:w-2/5 xl:w-1/4 min-h-screen md:sticky md:top-0 flex flex-col gap-4 justify-center items-center px-8 my-auto py-8 shadow-reverse font-bold text-gray-600 z-40`}>
+      <header
+        className={`${
+          !menuOpen ? "translate-x-[-100%] md:translate-x-0" : "translate-x-0"
+        } transition-all duration-500 bg-white w-full bottom-0 left-0 fixed md:w-2/5 xl:w-1/4 min-h-screen md:sticky md:top-0 flex flex-col gap-4 justify-center items-center px-8 my-auto py-8 shadow-reverse font-bold text-gray-600 z-40`}
+      >
         <Link
           className="flex flex top-auto items-center justify-center gap-2 hover:opacity-50 transition-all duration-500 me-auto"
           href="/"
@@ -61,86 +79,111 @@ export default function Navigation() {
           </p>
         </Link>
         <nav className="flex flex-col gap-0 items-start w-full">
-        <p className="text-xs uppercase mt-8 mb-4 bg-gradient-to-br from-teal-500 to-indigo-600 text-transparent bg-clip-text">Marketing</p>
-        <ul>
-        <Link
-            href="/marketing-services"
-            className={`${pathname.includes('marketing') ? 'text-teal-500': 'text-auto' } flex flex-row text-center md:text-left text-xs md:text-base items-center gap-2 transition-all duration-500 hover:opacity-50`}
-          >
-            <FcPositiveDynamic size={24} title="Marketing Services" />
+          <p className="text-xs uppercase mt-8 mb-4 bg-gradient-to-br from-teal-500 to-indigo-600 text-transparent bg-clip-text">
+            Marketing
+          </p>
+          <ul>
             <li className={`list-none py-2.5 text-sm cursor-pointer`}>
-              Marketing Services
+              <Link
+                href="/marketing-services"
+                className={`${
+                  pathname.includes("marketing") ? "text-teal-500" : "text-auto"
+                } flex flex-row text-center md:text-left text-xs md:text-base items-center gap-2 transition-all duration-500 hover:opacity-50`}
+              >
+                <FcPositiveDynamic size={24} title="Marketing Services" />
+                Marketing Services
+              </Link>
             </li>
-          </Link>
-          {pathname.includes('marketing') ?
-          <>
-          <Link
-            href="/marketing-services/content-marketing"
-            className={`${pathname.includes('content') ? 'text-teal-500': 'text-auto' } flex flex-row text-center md:text-left text-xs md:text-base items-center gap-2 transition-all duration-500 hover:opacity-50 ps-8`}
-          >
-            <FcFile size={24} title="Content Marketing" />
+            {pathname.includes("marketing") ? (
+              <>
+                  <li className={`list-none py-2.5 text-sm cursor-pointer`}>
+                <Link
+                  href="/marketing-services/content-marketing"
+                  className={`${
+                    pathname.includes("content") ? "text-teal-500" : "text-auto"
+                  } flex flex-row text-center md:text-left text-xs md:text-base items-center gap-2 transition-all duration-500 hover:opacity-50 ps-8`}
+                >
+                  <FcFile size={24} title="Content Marketing" />
+                    Content Marketing
+                </Link>
+                  </li>
+                  <li className={`list-none py-2.5 text-sm cursor-pointer`}>
+                <Link
+                  href="/marketing-services/organic-seo"
+                  className={`${
+                    pathname.includes("organic-seo")
+                      ? "text-teal-500"
+                      : "text-auto"
+                  } flex flex-row text-center md:text-left text-xs md:text-base items-center gap-2 transition-all duration-500 hover:opacity-50 ps-8`}
+                >
+                  <FcSearch size={24} title="Organic SEO" />
+                    Organic SEO
+                </Link>
+                  </li>
+                <li className={`list-none py-2.5 text-sm cursor-pointer`}>
+                <Link
+                  href="/marketing-services/design-services"
+                  className={`${
+                    pathname.includes("design-services")
+                      ? "text-teal-500"
+                      : "text-auto"
+                  } flex flex-row text-center md:text-left text-xs md:text-base items-center gap-2 transition-all duration-500 hover:opacity-50 ps-8`}
+                >
+                  <FcAddImage size={24} title="Design Services" />
+                    Design Services
+                </Link>
+                  </li>
+              </>
+            ) : null}
+            <p className="text-xs uppercase mt-8 mb-4 bg-gradient-to-br from-teal-500 to-indigo-600 text-transparent bg-clip-text">
+              Web Development
+            </p>
             <li className={`list-none py-2.5 text-sm cursor-pointer`}>
-              Content Marketing
-            </li>
-          </Link>
-          <Link
-            href="/marketing-services/organic-seo"
-            className={`${pathname.includes('organic-seo') ? 'text-teal-500': 'text-auto' } flex flex-row text-center md:text-left text-xs md:text-base items-center gap-2 transition-all duration-500 hover:opacity-50 ps-8`}
-          >
-            <FcSearch size={24} title="Organic SEO" />
-            <li className={`list-none py-2.5 text-sm cursor-pointer`}>
-              Organic SEO
-            </li>
-          </Link>
-          <Link
-            href="/marketing-services/design-services"
-            className={`${pathname.includes('design-services') ? 'text-teal-500': 'text-auto' } flex flex-row text-center md:text-left text-xs md:text-base items-center gap-2 transition-all duration-500 hover:opacity-50 ps-8`}
-          >
-            <FcAddImage size={24} title="Design Services" />
-            <li className={`list-none py-2.5 text-sm cursor-pointer`}>
-              Design Services
-            </li>
-          </Link>
-          </>
-          : null
-          }
-          <p className="text-xs uppercase mt-8 mb-4 bg-gradient-to-br from-teal-500 to-indigo-600 text-transparent bg-clip-text">Web Development</p>
-          <Link
-            href="/react-websites"
-            className={`${pathname.includes('react-websites') ? 'text-teal-500': 'text-auto' } flex flex-row text-center md:text-left text-xs md:text-base items-center gap-2 transition-all duration-500 hover:opacity-50`}
-          >
-            <FcFlashOn size={24} title="React framework websites" />
-            <li className={`list-none py-2.5 text-sm cursor-pointer`}>
-              Fast React Websites
-            </li>
-          </Link>
-          <Link
-            href="/web-apps"
-            className={`${pathname.includes('web-apps') ? 'text-teal-500': 'text-auto' } flex flex-row text-center md:text-left text-xs md:text-base items-center gap-2 transition-all duration-500 hover:opacity-50`}
-          >
-            <FcPhoneAndroid size={24} title="Web Apps" />
-            <li className={`list-none py-2.5 text-sm cursor-pointer`}>
-              Web Apps
-            </li>
-          </Link>
-          <Link
-            href="/wordpress-websites"
-            className={`${pathname.includes('wordpress') ? 'text-teal-500': 'text-auto' } flex flex-row text-center md:text-left text-xs md:text-base items-center gap-2 transition-all duration-500 hover:opacity-50`}
-          >
-            <BsWordpress size={24} title="Wordpress Development" />
-            <li className={`list-none py-2.5 text-sm cursor-pointer`}>
-              WordPress Development
-            </li>
-          </Link>
-          <Link
-            href="/shopify-sites"
-            className={`${pathname.includes('shopify') ? 'text-teal-500': 'text-auto' } flex flex-row text-center md:text-left text-xs md:text-base items-center gap-2 transition-all duration-500 hover:opacity-50`}
-          >
-            <FaShopify size={24} title="Shopify Sites" />
-            <li className={`list-none py-2.5 text-sm cursor-pointer`}>
-              Shopify Sites
-            </li>
-          </Link>
+            <Link
+              href="/react-websites"
+              className={`${
+                pathname.includes("react-websites")
+                  ? "text-teal-500"
+                  : "text-auto"
+              } flex flex-row text-center md:text-left text-xs md:text-base items-center gap-2 transition-all duration-500 hover:opacity-50`}
+            >
+              <FcFlashOn size={24} title="React framework websites" />
+                Fast React Websites
+            </Link>
+              </li>
+              <li className={`list-none py-2.5 text-sm cursor-pointer`}>
+            <Link
+              href="/web-apps"
+              className={`${
+                pathname.includes("web-apps") ? "text-teal-500" : "text-auto"
+              } flex flex-row text-center md:text-left text-xs md:text-base items-center gap-2 transition-all duration-500 hover:opacity-50`}
+            >
+              <FcPhoneAndroid size={24} title="Web Apps" />
+                Web Apps
+            </Link>
+              </li>
+              <li className={`list-none py-2.5 text-sm cursor-pointer`}>
+            <Link
+              href="/wordpress-websites"
+              className={`${
+                pathname.includes("wordpress") ? "text-teal-500" : "text-auto"
+              } flex flex-row text-center md:text-left text-xs md:text-base items-center gap-2 transition-all duration-500 hover:opacity-50`}
+            >
+              <BsWordpress size={24} title="Wordpress Development" />
+                WordPress Development
+            </Link>
+              </li>
+              <li className={`list-none py-2.5 text-sm cursor-pointer`}>
+            <Link
+              href="/shopify-sites"
+              className={`${
+                pathname.includes("shopify") ? "text-teal-500" : "text-auto"
+              } flex flex-row text-center md:text-left text-xs md:text-base items-center gap-2 transition-all duration-500 hover:opacity-50`}
+            >
+              <FaShopify size={24} title="Shopify Sites" />
+                Shopify Sites
+            </Link>
+              </li>
           </ul>
         </nav>
         <section className="md:border-t md:border-t-2 w-full pt-8 flex flex-wrap gap-4 items-center justify-between">
@@ -155,20 +198,29 @@ export default function Navigation() {
               target="_blank"
               className="hover:text-teal-500 transition-all duration-500"
             >
-              <TbBrandGithub aria-label="A link to Oliver's GitHub Profile" size={24} />
+              <TbBrandGithub
+                aria-label="A link to Oliver's GitHub Profile"
+                size={24}
+              />
             </Link>
             <Link
               href="https://www.linkedin.com/in/oliver-smith-software/"
               target="_blank"
               className="hover:text-teal-500 transition-all duration-500"
             >
-              <TbBrandLinkedin aria-label="A link to Oliver's LinkedIn Profile" size={24} />
+              <TbBrandLinkedin
+                aria-label="A link to Oliver's LinkedIn Profile"
+                size={24}
+              />
             </Link>
             <Link
               href="/contact"
               className="hover:text-teal-500 transition-all duration-500"
             >
-              <TbMail aria-label="A link to a contact form to get in touch with Oliver" size={24} />
+              <TbMail
+                aria-label="A link to a contact form to get in touch with Oliver"
+                size={24}
+              />
             </Link>
           </div>
         </section>
